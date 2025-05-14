18b1967218b2733fbd8ee4d175969b1c22b7929d
I_DrinkEggs <aki01teru@gmail.com>
2025-05-14 20:29:06 +0900

    My first commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    #In python if __functionName__ is used, then it's a private function
+    #If the functionName is a data type, it's a function that casts the input into that data type
+    def __str__(self): 
+        return self.title
